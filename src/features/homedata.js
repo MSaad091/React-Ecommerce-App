b9970dbe4f0eded0f8af20a1b263c59196a06e8f
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-01-18 09:33:23 -0800

    Fix image paths for Vercel

--- src/features/homedata.js
+++ src/features/homedata.js
@@ -1,74 +1,125 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const homedataSlice = createSlice({
+//    name:'homedata',
+//    initialState: {
+//     products:[
+//         {
+//             id:"1",
+//             name:"Shoes",
+//             bname:"Nike",
+//             img: '/public/banner.jpg',
+//             price:"2000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"2",
+//             name:"Shoes",
+//             bname:"Nike",
+//            img: '/public/sheoes3 (2).jpg',
+//             price:"3000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"3",
+//             name:"Shoes",
+//             bname:"Nike",
+//            img: '/public/shoes1 (3).jpg',
+//             price:"4000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"4",
+//             name:"Shoes",
+//             bname:"Nike",
+//        img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         },
+//           {
+//             id:"5",
+//             name:"Shoes",
+//             bname:"Nike",
+//            img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         } , {
+//             id:"6",
+//             name:"Shoes",
+//             bname:"Nike",
+//        img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         },
+//          {
+//             id:"7",
+//             name:"Bage",
+//             bname:"Addidad",
+//        img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"8",
+//             name:"HeadPhone",
+//             bname:"Audionic",
+//        img: '/public//banner2.jpg',
+//             price:"8000",
+//             info: '10% Off This HeadPhone'
+//         },
+       
+//     ]
+//    }
+// })
+// export default homedataSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const homedataSlice = createSlice({
-   name:'homedata',
-   initialState: {
-    products:[
-        {
-            id:"1",
-            name:"Shoes",
-            bname:"Nike",
-            img: '/public/banner.jpg',
-            price:"2000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"2",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/sheoes3 (2).jpg',
-            price:"3000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"3",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/shoes1 (3).jpg',
-            price:"4000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"4",
-            name:"Shoes",
-            bname:"Nike",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-          {
-            id:"5",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        } , {
-            id:"6",
-            name:"Shoes",
-            bname:"Nike",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-         {
-            id:"7",
-            name:"Bage",
-            bname:"Addidad",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"8",
-            name:"HeadPhone",
-            bname:"Audionic",
-       img: '/public//banner2.jpg',
-            price:"8000",
-            info: '10% Off This HeadPhone'
-        },
-       
-    ]
-   }
-})
+  name: "homedata",
+  initialState: {
+    products: [
+      {
+        id: "1",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/banner.jpg",
+        price: "2000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "2",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes3.jpg",
+        price: "3000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "3",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes1.jpg",
+        price: "4000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "4",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes2.jpg",
+        price: "5000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "8",
+        name: "HeadPhone",
+        bname: "Audionic",
+        img: "/banner2.jpg",
+        price: "8000",
+        info: "10% Off This HeadPhone",
+      },
+    ],
+  },
+});
+
 export default homedataSlice.reducer;
--- src/features/shopdata.js
+++ src/features/shopdata.js
@@ -1,74 +1,133 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const ShopSlice = createSlice({
+//     name: 'shopdata',
+//     initialState: {
+//         Shopproduct: [
+//            {
+//             id:"1",
+//             name:"Shoes",
+//             bname:"Nike",
+//             img: '/public/sheoes3 (1).jpg',
+//             price:"2000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"2",
+//             name:"Shoes",
+//             bname:"Nike",
+//            img: '/public/sheoes3 (2).jpg',
+//             price:"3000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"3",
+//             name:"Shoes",
+//             bname:"Nike",
+//            img: '/public/shoes1 (3).jpg',
+//             price:"4000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"4",
+//             name:"Shoes",
+//             bname:"Nike",
+//        img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         },
+//           {
+//             id:"5",
+//             name:"Shoes",
+//             bname:"Nike",
+//            img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         } , {
+//             id:"6",
+//             name:"Shoes",
+//             bname:"Nike",
+//        img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         },
+//          {
+//             id:"7",
+//             name:"Bage",
+//             bname:"Addidad",
+//        img: '/public/shoes1 (1).jpg',
+//             price:"5000",
+//             info: '50% Off This Shoes'
+//         },
+//         {
+//             id:"8",
+//             name:"HeadPhone",
+//             bname:"Audionic",
+//        img: '/public//banner2.jpg',
+//             price:"8000",
+//             info: '10% Off This HeadPhone'
+//         },
+//         ]
+//     }
+// });
+
+// export default ShopSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
 
 const ShopSlice = createSlice({
-    name: 'shopdata',
-    initialState: {
-        Shopproduct: [
-           {
-            id:"1",
-            name:"Shoes",
-            bname:"Nike",
-            img: '/public/sheoes3 (1).jpg',
-            price:"2000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"2",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/sheoes3 (2).jpg',
-            price:"3000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"3",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/shoes1 (3).jpg',
-            price:"4000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"4",
-            name:"Shoes",
-            bname:"Nike",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-          {
-            id:"5",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        } , {
-            id:"6",
-            name:"Shoes",
-            bname:"Nike",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-         {
-            id:"7",
-            name:"Bage",
-            bname:"Addidad",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"8",
-            name:"HeadPhone",
-            bname:"Audionic",
-       img: '/public//banner2.jpg',
-            price:"8000",
-            info: '10% Off This HeadPhone'
-        },
-        ]
-    }
+  name: "shopdata",
+  initialState: {
+    Shopproduct: [
+      {
+        id: "1",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes3.jpg",
+        price: "2000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "2",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes2.jpg",
+        price: "3000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "3",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes1.jpg",
+        price: "4000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "4",
+        name: "Shoes",
+        bname: "Nike",
+        img: "/shoes1.jpg",
+        price: "5000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "7",
+        name: "Bag",
+        bname: "Adidas",
+        img: "/shoes1.jpg",
+        price: "5000",
+        info: "50% Off This Shoes",
+      },
+      {
+        id: "8",
+        name: "HeadPhone",
+        bname: "Audionic",
+        img: "/banner2.jpg",
+        price: "8000",
+        info: "10% Off This HeadPhone",
+      },
+    ],
+  },
 });
 
 export default ShopSlice.reducer;
