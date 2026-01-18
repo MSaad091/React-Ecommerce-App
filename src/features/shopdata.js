b872420c813d1f4c0d8bf539b43859c1658c3349
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-01-17 16:15:46 -0800

    Fix: Images paths for deployment

--- src/features/shopdata.js
+++ src/features/shopdata.js
@@ -1,68 +1,67 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const ShopSlice = createSlice({
-    name:'shopdata',
-    initialState:{
-        Shopproduct:[
+    name: 'shopdata',
+    initialState: {
+        Shopproduct: [
             {
-                 
-            id:"1",
-            name:"Shoes",
-            bname:"Nike",
-            img: '/public/sheoes3 (1).jpg',
-            price:"2000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"2",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/sheoes3 (2).jpg',
-            price:"3000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"3",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/shoes1 (3).jpg',
-            price:"4000",
-            info: '50% Off This Shoes'
-        },
-        {
-            id:"5",
-            name:"Shoes",
-            bname:"Nike",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-          {
-            id:"6",
-            name:"Shoes",
-            bname:"Nike",
-           img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        } , {
-            id:"7",
-            name:"Shoes",
-            bname:"Nike",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-          {
-            id:"8",
-            name:"bage",
-            bname:"Addidad",
-       img: '/public/shoes1 (1).jpg',
-            price:"5000",
-            info: '50% Off This Shoes'
-        },
-            
+                id: "1",
+                name: "Shoes",
+                bname: "Nike",
+                img: '/images/sheoes3-1.jpg',
+                price: "2000",
+                info: '50% Off This Shoes'
+            },
+            {
+                id: "2",
+                name: "Shoes",
+                bname: "Nike",
+                img: '/images/sheoes3-2.jpg',
+                price: "3000",
+                info: '50% Off This Shoes'
+            },
+            {
+                id: "3",
+                name: "Shoes",
+                bname: "Nike",
+                img: '/images/shoes1-3.jpg',
+                price: "4000",
+                info: '50% Off This Shoes'
+            },
+            {
+                id: "5",
+                name: "Shoes",
+                bname: "Nike",
+                img: '/images/shoes1-1.jpg',
+                price: "5000",
+                info: '50% Off This Shoes'
+            },
+            {
+                id: "6",
+                name: "Shoes",
+                bname: "Nike",
+                img: '/images/shoes1-1.jpg',
+                price: "5000",
+                info: '50% Off This Shoes'
+            },
+            {
+                id: "7",
+                name: "Shoes",
+                bname: "Nike",
+                img: '/images/shoes1-1.jpg',
+                price: "5000",
+                info: '50% Off This Shoes'
+            },
+            {
+                id: "8",
+                name: "Bage",
+                bname: "Adidas",
+                img: '/images/shoes1-1.jpg',
+                price: "5000",
+                info: '50% Off This Bage'
+            }
         ]
     }
+});
 
-})
-export default ShopSlice.reducer
+export default ShopSlice.reducer;
